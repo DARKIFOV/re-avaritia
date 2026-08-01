33566baa289ff7aa400e02c1613a99a720e3ca86
DARKIFOV <seleznev.ivan2005@gmail.com>
2026-08-02 02:55:36 +0500

    Register restored energy food, neutronium and luck items

--- src/main/java/ru/rfvv/metatechreborn/registry/ModItems.java
+++ src/main/java/ru/rfvv/metatechreborn/registry/ModItems.java
@@ -9,8 +9,12 @@ import net.minecraftforge.registries.RegistryObject;
 import ru.rfvv.metatechreborn.MetaTechReborn;
 import ru.rfvv.metatechreborn.item.BlankExtremePatternItem;
 import ru.rfvv.metatechreborn.item.EncodedExtremePatternItem;
+import ru.rfvv.metatechreborn.item.EnergyFoodItem;
 import ru.rfvv.metatechreborn.item.GreenhouseModuleItem;
+import ru.rfvv.metatechreborn.item.LuckConverterUpgradeItem;
+import ru.rfvv.metatechreborn.item.LuckModuleItem;
 import ru.rfvv.metatechreborn.item.ManaDrillUpgradeItem;
+import ru.rfvv.metatechreborn.item.NeutroniumCombinerUpgradeItem;
 import ru.rfvv.metatechreborn.item.PatternCapacityUpgradeItem;
 
 import java.util.List;
@@ -21,6 +25,12 @@ public final class ModItems {
 
     public static final RegistryObject<Item> MOLECULAR_ASSEMBLER_9X9 = blockItem(
             "molecular_assembler_9x9", ModBlocks.MOLECULAR_ASSEMBLER_9X9);
+    public static final RegistryObject<Item> NEUTRONIUM_COMBINER = blockItem(
+            "neutronium_combiner", ModBlocks.NEUTRONIUM_COMBINER);
+    public static final RegistryObject<Item> LUCK_CONVERTER = blockItem(
+            "luck_converter", ModBlocks.LUCK_CONVERTER);
+    public static final RegistryObject<Item> ADVANCED_LUCK_CONVERTER = blockItem(
+            "advanced_luck_converter", ModBlocks.ADVANCED_LUCK_CONVERTER);
     public static final RegistryObject<Item> MANA_DRILL = blockItem(
             "mana_drill", ModBlocks.MANA_DRILL);
     public static final RegistryObject<Item> MANA_DRILL_CASING = blockItem(
@@ -39,9 +49,53 @@ public final class ModItems {
     public static final RegistryObject<Item> PATTERN_CAPACITY_UPGRADE = ITEMS.register(
             "pattern_capacity_upgrade", PatternCapacityUpgradeItem::new);
 
+    public static final RegistryObject<Item> NEUTRON_COMBINER_SPEED_UPGRADE = ITEMS.register(
+            "neutron_combiner_speed_upgrade",
+            () -> new NeutroniumCombinerUpgradeItem(NeutroniumCombinerUpgradeItem.Type.SPEED));
+    public static final RegistryObject<Item> NEUTRON_COMBINER_EFFICIENCY_UPGRADE = ITEMS.register(
+            "neutron_combiner_efficiency_upgrade",
+            () -> new NeutroniumCombinerUpgradeItem(NeutroniumCombinerUpgradeItem.Type.EFFICIENCY));
+    public static final RegistryObject<Item> NEUTRON_COMBINER_OUTPUT_UPGRADE = ITEMS.register(
+            "neutron_combiner_output_upgrade",
+            () -> new NeutroniumCombinerUpgradeItem(NeutroniumCombinerUpgradeItem.Type.OUTPUT));
+
+    public static final RegistryObject<Item> ENERGY_FOOD_TIER_1 = energyFood(
+            "energy_food_tier_1", 1, 1.0F, 32, 150, 50_000, 500, 1);
+    public static final RegistryObject<Item> ENERGY_FOOD_TIER_2 = energyFood(
+            "energy_food_tier_2", 2, 2.0F, 16, 300, 100_000, 1_000, 2);
+    public static final RegistryObject<Item> ENERGY_FOOD_TIER_3 = energyFood(
+            "energy_food_tier_3", 3, 3.0F, 1, 450, 150_000, 1_500, 3);
+    public static final RegistryObject<Item> ENERGY_FOOD_TIER_4 = energyFood(
+            "energy_food_tier_4", 20, 20.0F, 0, 600, 300_000, 3_000, 4);
+
+    public static final RegistryObject<Item> LUCK_MODULE_1 = luckModule("luck_module_1", 1);
+    public static final RegistryObject<Item> LUCK_MODULE_2 = luckModule("luck_module_2", 2);
+    public static final RegistryObject<Item> LUCK_MODULE_3 = luckModule("luck_module_3", 3);
+    public static final RegistryObject<Item> LUCK_MODULE_4 = luckModule("luck_module_4", 5);
+    public static final RegistryObject<Item> LUCK_MODULE_5 = luckModule("luck_module_5", 10);
+    public static final RegistryObject<Item> LUCK_MODULE_6 = luckModule("luck_module_6", 15);
+    public static final RegistryObject<Item> LUCK_MODULE_7 = luckModule("luck_module_7", 20);
+    public static final RegistryObject<Item> LUCK_MODULE_8 = luckModule("luck_module_8", 25);
+    public static final RegistryObject<Item> LUCK_MODULE_9 = luckModule("luck_module_9", 30);
+    public static final RegistryObject<Item> LUCK_MODULE_10 = luckModule("luck_module_10", 35);
+    public static final RegistryObject<Item> LUCK_MODULE_11 = luckModule("luck_module_11", 40);
+    public static final RegistryObject<Item> LUCK_MODULE_12 = luckModule("luck_module_12", 50);
+
+    public static final RegistryObject<Item> LUCK_SPEED_UPGRADE = luckUpgrade(
+            "luck_speed_upgrade", LuckConverterUpgradeItem.Type.SPEED);
+    public static final RegistryObject<Item> LUCK_EFFICIENCY_UPGRADE = luckUpgrade(
+            "luck_efficiency_upgrade", LuckConverterUpgradeItem.Type.EFFICIENCY);
+    public static final RegistryObject<Item> LUCK_OPERATIONS_UPGRADE = luckUpgrade(
+            "luck_operations_upgrade", LuckConverterUpgradeItem.Type.OPERATIONS);
+    public static final RegistryObject<Item> LUCK_DOUBLE_UPGRADE = luckUpgrade(
+            "luck_double_upgrade", LuckConverterUpgradeItem.Type.DOUBLE);
+    public static final RegistryObject<Item> LUCK_SMELT_UPGRADE = luckUpgrade(
+            "luck_smelt_upgrade", LuckConverterUpgradeItem.Type.SMELT);
+    public static final RegistryObject<Item> LUCK_AUTO_EJECT_UPGRADE = luckUpgrade(
+            "luck_auto_eject_upgrade", LuckConverterUpgradeItem.Type.AUTO_EJECT);
+
     public static final RegistryObject<Item> MANA_DRILL_MODULE = ITEMS.register(
             "mana_drill_module", () -> new Item(new Item.Properties().stacksTo(1)));
-
     public static final RegistryObject<Item> MANA_DRILL_SPEED_UPGRADE = upgrade(
             "mana_drill_speed_upgrade", ManaDrillUpgradeItem.Type.SPEED, 1);
     public static final RegistryObject<Item> MANA_DRILL_SPEED_UPGRADE_2 = upgrade(
@@ -52,7 +106,6 @@ public final class ModItems {
             "mana_drill_speed_upgrade_4", ManaDrillUpgradeItem.Type.SPEED, 4);
     public static final RegistryObject<Item> MANA_DRILL_SPEED_UPGRADE_5 = upgrade(
             "mana_drill_speed_upgrade_5", ManaDrillUpgradeItem.Type.SPEED, 5);
-
     public static final RegistryObject<Item> MANA_DRILL_LOOTING_UPGRADE = upgrade(
             "mana_drill_looting_upgrade", ManaDrillUpgradeItem.Type.LOOTING, 1);
     public static final RegistryObject<Item> MANA_DRILL_LOOTING_UPGRADE_2 = upgrade(
@@ -71,7 +124,6 @@ public final class ModItems {
             "mana_drill_looting_upgrade_8", ManaDrillUpgradeItem.Type.LOOTING, 8);
     public static final RegistryObject<Item> MANA_DRILL_LOOTING_UPGRADE_9 = upgrade(
             "mana_drill_looting_upgrade_9", ManaDrillUpgradeItem.Type.LOOTING, 9);
-
     public static final RegistryObject<Item> MANA_DRILL_GENERATION_UPGRADE = upgrade(
             "mana_drill_generation_upgrade", ManaDrillUpgradeItem.Type.GENERATION, 1);
     public static final RegistryObject<Item> MANA_DRILL_GENERATION_UPGRADE_2 = upgrade(
@@ -104,42 +156,58 @@ public final class ModItems {
     public static final RegistryObject<Item> GREENHOUSE_INFINITE_LAVA_MODULE = greenhouseModule(
             "greenhouse_infinite_lava_module", GreenhouseModuleItem.Type.INFINITE_LAVA, 1);
 
-    private static RegistryObject<Item> blockItem(
-            String name, RegistryObject<net.minecraft.world.level.block.Block> block) {
+    private static RegistryObject<Item> blockItem(String name,
+            RegistryObject<net.minecraft.world.level.block.Block> block) {
         return ITEMS.register(name, () -> new BlockItem(block.get(), new Item.Properties()));
     }
-
     private static RegistryObject<Item> upgrade(String name, ManaDrillUpgradeItem.Type type, int level) {
         return ITEMS.register(name, () -> new ManaDrillUpgradeItem(type, level));
     }
-
-    private static RegistryObject<Item> greenhouseModule(
-            String name, GreenhouseModuleItem.Type type, int level) {
+    private static RegistryObject<Item> greenhouseModule(String name, GreenhouseModuleItem.Type type, int level) {
         return ITEMS.register(name, () -> new GreenhouseModuleItem(type, level));
     }
+    private static RegistryObject<Item> energyFood(String name, int nutrition, float saturation, int duration,
+            int cost, int capacity, int transfer, int tier) {
+        return ITEMS.register(name, () -> new EnergyFoodItem(nutrition, saturation, duration,
+                cost, capacity, transfer, tier));
+    }
+    private static RegistryObject<Item> luckModule(String name, int level) {
+        return ITEMS.register(name, () -> new LuckModuleItem(level));
+    }
+    private static RegistryObject<Item> luckUpgrade(String name, LuckConverterUpgradeItem.Type type) {
+        return ITEMS.register(name, () -> new LuckConverterUpgradeItem(type));
+    }
 
+    public static List<RegistryObject<Item>> neutronItems() {
+        return List.of(NEUTRON_COMBINER_SPEED_UPGRADE, NEUTRON_COMBINER_EFFICIENCY_UPGRADE,
+                NEUTRON_COMBINER_OUTPUT_UPGRADE);
+    }
+    public static List<RegistryObject<Item>> energyFoodItems() {
+        return List.of(ENERGY_FOOD_TIER_1, ENERGY_FOOD_TIER_2, ENERGY_FOOD_TIER_3, ENERGY_FOOD_TIER_4);
+    }
+    public static List<RegistryObject<Item>> luckModuleItems() {
+        return List.of(LUCK_MODULE_1, LUCK_MODULE_2, LUCK_MODULE_3, LUCK_MODULE_4, LUCK_MODULE_5,
+                LUCK_MODULE_6, LUCK_MODULE_7, LUCK_MODULE_8, LUCK_MODULE_9, LUCK_MODULE_10,
+                LUCK_MODULE_11, LUCK_MODULE_12);
+    }
+    public static List<RegistryObject<Item>> luckUpgradeItems() {
+        return List.of(LUCK_SPEED_UPGRADE, LUCK_EFFICIENCY_UPGRADE, LUCK_OPERATIONS_UPGRADE,
+                LUCK_DOUBLE_UPGRADE, LUCK_SMELT_UPGRADE, LUCK_AUTO_EJECT_UPGRADE);
+    }
     public static List<RegistryObject<Item>> manaDrillUpgradeItems() {
-        return List.of(
-                MANA_DRILL_SPEED_UPGRADE, MANA_DRILL_SPEED_UPGRADE_2, MANA_DRILL_SPEED_UPGRADE_3,
+        return List.of(MANA_DRILL_SPEED_UPGRADE, MANA_DRILL_SPEED_UPGRADE_2, MANA_DRILL_SPEED_UPGRADE_3,
                 MANA_DRILL_SPEED_UPGRADE_4, MANA_DRILL_SPEED_UPGRADE_5,
                 MANA_DRILL_LOOTING_UPGRADE, MANA_DRILL_LOOTING_UPGRADE_2, MANA_DRILL_LOOTING_UPGRADE_3,
                 MANA_DRILL_LOOTING_UPGRADE_4, MANA_DRILL_LOOTING_UPGRADE_5, MANA_DRILL_LOOTING_UPGRADE_6,
                 MANA_DRILL_LOOTING_UPGRADE_7, MANA_DRILL_LOOTING_UPGRADE_8, MANA_DRILL_LOOTING_UPGRADE_9,
-                MANA_DRILL_GENERATION_UPGRADE, MANA_DRILL_GENERATION_UPGRADE_2,
-                MANA_DRILL_GENERATION_UPGRADE_3
-        );
+                MANA_DRILL_GENERATION_UPGRADE, MANA_DRILL_GENERATION_UPGRADE_2, MANA_DRILL_GENERATION_UPGRADE_3);
     }
-
     public static List<RegistryObject<Item>> greenhouseModuleItems() {
-        return List.of(
-                GREENHOUSE_ECONOMY_MODULE_1, GREENHOUSE_ECONOMY_MODULE_2, GREENHOUSE_ECONOMY_MODULE_3,
+        return List.of(GREENHOUSE_ECONOMY_MODULE_1, GREENHOUSE_ECONOMY_MODULE_2, GREENHOUSE_ECONOMY_MODULE_3,
                 GREENHOUSE_EFFICIENCY_MODULE_1, GREENHOUSE_EFFICIENCY_MODULE_2, GREENHOUSE_EFFICIENCY_MODULE_3,
                 GREENHOUSE_SPEED_MODULE_1, GREENHOUSE_SPEED_MODULE_2, GREENHOUSE_SPEED_MODULE_3,
-                GREENHOUSE_INFINITE_DAY_MODULE, GREENHOUSE_INFINITE_NIGHT_MODULE,
-                GREENHOUSE_INFINITE_LAVA_MODULE
-        );
+                GREENHOUSE_INFINITE_DAY_MODULE, GREENHOUSE_INFINITE_NIGHT_MODULE, GREENHOUSE_INFINITE_LAVA_MODULE);
     }
-
     public static List<RegistryObject<Item>> nativePatternItems() {
         return List.of(BLANK_EXTREME_PATTERN, ENCODED_EXTREME_PATTERN, PATTERN_CAPACITY_UPGRADE);
     }
