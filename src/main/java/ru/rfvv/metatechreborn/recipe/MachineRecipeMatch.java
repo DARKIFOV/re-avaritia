d9b0056e42a7248ad8b513119a135fc304b053b2
DARKIFOV <seleznev.ivan2005@gmail.com>
2026-08-01 16:03:45 +0500

    Add normalized machine recipe match

---
+++ src/main/java/ru/rfvv/metatechreborn/recipe/MachineRecipeMatch.java
@@ -0,0 +1,10 @@
+package ru.rfvv.metatechreborn.recipe;
+
+import net.minecraft.core.NonNullList;
+import net.minecraft.resources.ResourceLocation;
+import net.minecraft.world.item.ItemStack;
+
+public record MachineRecipeMatch(ResourceLocation id, Source source, ItemStack result,
+        NonNullList<ItemStack> remainingItems, int craftTime, int energyPerTick) {
+    public enum Source { METATECH, AVARITIA }
+}
