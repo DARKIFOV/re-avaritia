7a4fdcfc2a5f54bf79ad820e3c5e1f69adef38ac
DARKIFOV <seleznev.ivan2005@gmail.com>
2026-08-02 03:00:21 +0500

    Register neutronium and luck converter screens

--- src/main/java/ru/rfvv/metatechreborn/client/ClientModEvents.java
+++ src/main/java/ru/rfvv/metatechreborn/client/ClientModEvents.java
@@ -8,8 +8,10 @@ import net.minecraftforge.fml.event.lifecycle.FMLClientSetupEvent;
 import ru.rfvv.metatechreborn.MetaTechReborn;
 import ru.rfvv.metatechreborn.client.screen.ExtremePatternEncoderScreen;
 import ru.rfvv.metatechreborn.client.screen.GreenhouseScreen;
+import ru.rfvv.metatechreborn.client.screen.LuckConverterScreen;
 import ru.rfvv.metatechreborn.client.screen.ManaDrillScreen;
 import ru.rfvv.metatechreborn.client.screen.MolecularAssemblerScreen;
+import ru.rfvv.metatechreborn.client.screen.NeutroniumCombinerScreen;
 import ru.rfvv.metatechreborn.registry.ModMenus;
 
 @Mod.EventBusSubscriber(modid = MetaTechReborn.MOD_ID, bus = Mod.EventBusSubscriber.Bus.MOD, value = Dist.CLIENT)
@@ -19,10 +21,11 @@ public final class ClientModEvents {
         event.enqueueWork(() -> {
             MenuScreens.register(ModMenus.MOLECULAR_ASSEMBLER_9X9.get(), MolecularAssemblerScreen::new);
             MenuScreens.register(ModMenus.EXTREME_PATTERN_ENCODER.get(), ExtremePatternEncoderScreen::new);
+            MenuScreens.register(ModMenus.NEUTRONIUM_COMBINER.get(), NeutroniumCombinerScreen::new);
+            MenuScreens.register(ModMenus.LUCK_CONVERTER.get(), LuckConverterScreen::new);
             MenuScreens.register(ModMenus.MANA_DRILL.get(), ManaDrillScreen::new);
             MenuScreens.register(ModMenus.GREENHOUSE.get(), GreenhouseScreen::new);
         });
     }
-
     private ClientModEvents() {}
 }
