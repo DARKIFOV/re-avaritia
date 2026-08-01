6b1189ca94bd0cb2c4461afb02dde9a23ca85a9c
DARKIFOV <seleznev.ivan2005@gmail.com>
2026-08-02 02:57:29 +0500

    Register neutronium and luck converter block entities

--- src/main/java/ru/rfvv/metatechreborn/registry/ModBlockEntities.java
+++ src/main/java/ru/rfvv/metatechreborn/registry/ModBlockEntities.java
@@ -8,26 +8,33 @@ import net.minecraftforge.registries.RegistryObject;
 import ru.rfvv.metatechreborn.MetaTechReborn;
 import ru.rfvv.metatechreborn.blockentity.ExtremePatternEncoderBlockEntity;
 import ru.rfvv.metatechreborn.blockentity.GreenhouseBlockEntity;
+import ru.rfvv.metatechreborn.blockentity.LuckConverterBlockEntity;
 import ru.rfvv.metatechreborn.blockentity.ManaDrillBlockEntity;
 import ru.rfvv.metatechreborn.blockentity.MolecularAssemblerBlockEntity;
+import ru.rfvv.metatechreborn.blockentity.NeutroniumCombinerBlockEntity;
 
 public final class ModBlockEntities {
     public static final DeferredRegister<BlockEntityType<?>> BLOCK_ENTITIES =
             DeferredRegister.create(ForgeRegistries.BLOCK_ENTITY_TYPES, MetaTechReborn.MOD_ID);
-
     public static final RegistryObject<BlockEntityType<MolecularAssemblerBlockEntity>> MOLECULAR_ASSEMBLER_9X9 =
             BLOCK_ENTITIES.register("molecular_assembler_9x9", () -> BlockEntityType.Builder.of(
                     MolecularAssemblerBlockEntity::new, ModBlocks.MOLECULAR_ASSEMBLER_9X9.get()).build(null));
     public static final RegistryObject<BlockEntityType<ExtremePatternEncoderBlockEntity>> EXTREME_PATTERN_ENCODER =
             BLOCK_ENTITIES.register("extreme_pattern_encoder", () -> BlockEntityType.Builder.of(
                     ExtremePatternEncoderBlockEntity::new, ModBlocks.EXTREME_PATTERN_ENCODER.get()).build(null));
+    public static final RegistryObject<BlockEntityType<NeutroniumCombinerBlockEntity>> NEUTRONIUM_COMBINER =
+            BLOCK_ENTITIES.register("neutronium_combiner", () -> BlockEntityType.Builder.of(
+                    NeutroniumCombinerBlockEntity::new, ModBlocks.NEUTRONIUM_COMBINER.get()).build(null));
+    public static final RegistryObject<BlockEntityType<LuckConverterBlockEntity>> LUCK_CONVERTER =
+            BLOCK_ENTITIES.register("luck_converter", () -> BlockEntityType.Builder.of(
+                    LuckConverterBlockEntity::new, ModBlocks.LUCK_CONVERTER.get(),
+                    ModBlocks.ADVANCED_LUCK_CONVERTER.get()).build(null));
     public static final RegistryObject<BlockEntityType<ManaDrillBlockEntity>> MANA_DRILL =
             BLOCK_ENTITIES.register("mana_drill", () -> BlockEntityType.Builder.of(
                     ManaDrillBlockEntity::new, ModBlocks.MANA_DRILL.get()).build(null));
     public static final RegistryObject<BlockEntityType<GreenhouseBlockEntity>> GREENHOUSE =
             BLOCK_ENTITIES.register("greenhouse", () -> BlockEntityType.Builder.of(
                     GreenhouseBlockEntity::new, ModBlocks.GREENHOUSE.get()).build(null));
-
     public static void register(IEventBus bus) { BLOCK_ENTITIES.register(bus); }
     private ModBlockEntities() {}
 }
