d0fa3d0f0b27888084f1910b6d87efa5327318b1
DARKIFOV <seleznev.ivan2005@gmail.com>
2026-08-01 22:14:45 +0500

    Register native pattern encoder screen

--- src/main/java/ru/rfvv/metatechreborn/client/ClientModEvents.java
+++ src/main/java/ru/rfvv/metatechreborn/client/ClientModEvents.java
@@ -8,6 +8,7 @@ import net.minecraftforge.eventbus.api.SubscribeEvent;
 import net.minecraftforge.fml.common.Mod;
 import net.minecraftforge.fml.event.lifecycle.FMLClientSetupEvent;
 import ru.rfvv.metatechreborn.MetaTechReborn;
+import ru.rfvv.metatechreborn.client.screen.ExtremePatternEncoderScreen;
 import ru.rfvv.metatechreborn.client.screen.GreenhouseScreen;
 import ru.rfvv.metatechreborn.client.screen.ManaDrillScreen;
 import ru.rfvv.metatechreborn.client.screen.MolecularAssemblerScreen;
@@ -25,6 +26,7 @@ public final class ClientModEvents {
     public static void clientSetup(FMLClientSetupEvent event) {
         event.enqueueWork(() -> {
             MenuScreens.register(ModMenus.MOLECULAR_ASSEMBLER_9X9.get(), MolecularAssemblerScreen::new);
+            MenuScreens.register(ModMenus.EXTREME_PATTERN_ENCODER.get(), ExtremePatternEncoderScreen::new);
             MenuScreens.register(ModMenus.NEUTRONIUM_COMBINER.get(), NeutroniumCombinerScreen::new);
             MenuScreens.register(ModMenus.MANA_DRILL.get(), ManaDrillScreen::new);
             MenuScreens.register(ModMenus.GREENHOUSE.get(), GreenhouseScreen::new);
