70e11473a70cd198ed6ee52b6e948a1488f12a87
DARKIFOV <seleznev.ivan2005@gmail.com>
2026-08-01 15:59:56 +0500

    Register machine block entities

---
+++ src/main/java/ru/rfvv/metatechreborn/registry/ModBlockEntities.java
@@ -0,0 +1,32 @@
+package ru.rfvv.metatechreborn.registry;
+
+import net.minecraft.world.level.block.entity.BlockEntityType;
+import net.minecraftforge.eventbus.api.IEventBus;
+import net.minecraftforge.registries.DeferredRegister;
+import net.minecraftforge.registries.ForgeRegistries;
+import net.minecraftforge.registries.RegistryObject;
+import ru.rfvv.metatechreborn.MetaTechReborn;
+import ru.rfvv.metatechreborn.blockentity.MolecularAssemblerBlockEntity;
+import ru.rfvv.metatechreborn.blockentity.NeutroniumCombinerBlockEntity;
+
+public final class ModBlockEntities {
+    public static final DeferredRegister<BlockEntityType<?>> BLOCK_ENTITIES =
+            DeferredRegister.create(ForgeRegistries.BLOCK_ENTITY_TYPES, MetaTechReborn.MOD_ID);
+
+    public static final RegistryObject<BlockEntityType<MolecularAssemblerBlockEntity>> MOLECULAR_ASSEMBLER_9X9 =
+            BLOCK_ENTITIES.register("molecular_assembler_9x9",
+                    () -> BlockEntityType.Builder.of(MolecularAssemblerBlockEntity::new,
+                            ModBlocks.MOLECULAR_ASSEMBLER_9X9.get()).build(null));
+
+    public static final RegistryObject<BlockEntityType<NeutroniumCombinerBlockEntity>> NEUTRONIUM_COMBINER =
+            BLOCK_ENTITIES.register("neutronium_combiner",
+                    () -> BlockEntityType.Builder.of(NeutroniumCombinerBlockEntity::new,
+                            ModBlocks.NEUTRONIUM_COMBINER.get()).build(null));
+
+    public static void register(IEventBus bus) {
+        BLOCK_ENTITIES.register(bus);
+    }
+
+    private ModBlockEntities() {
+    }
+}
