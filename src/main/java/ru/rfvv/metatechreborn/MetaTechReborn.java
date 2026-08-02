341dffe9bb61c2b6d76f06587c1b17e38c4c6bc6
DARKIFOV <seleznev.ivan2005@gmail.com>
2026-08-02 12:13:54 +0500

    Register native AE2 assembler provider when available

--- src/main/java/ru/rfvv/metatechreborn/MetaTechReborn.java
+++ src/main/java/ru/rfvv/metatechreborn/MetaTechReborn.java
@@ -4,12 +4,14 @@ import com.mojang.logging.LogUtils;
 import net.minecraft.world.item.CreativeModeTabs;
 import net.minecraftforge.event.BuildCreativeModeTabContentsEvent;
 import net.minecraftforge.eventbus.api.IEventBus;
+import net.minecraftforge.fml.ModList;
 import net.minecraftforge.fml.ModLoadingContext;
 import net.minecraftforge.fml.common.Mod;
 import net.minecraftforge.fml.config.ModConfig;
 import net.minecraftforge.fml.javafmlmod.FMLJavaModLoadingContext;
 import org.slf4j.Logger;
 import ru.rfvv.metatechreborn.config.CommonConfig;
+import ru.rfvv.metatechreborn.integration.ae2.MolecularAssemblerAe2Provider;
 import ru.rfvv.metatechreborn.registry.ModBlockEntities;
 import ru.rfvv.metatechreborn.registry.ModBlocks;
 import ru.rfvv.metatechreborn.registry.ModItems;
@@ -32,6 +34,7 @@ public final class MetaTechReborn {
         ModRecipes.register(modBus);
         modBus.addListener(this::addCreativeTabContents);
         ModLoadingContext.get().registerConfig(ModConfig.Type.COMMON, CommonConfig.SPEC);
+        if (ModList.get().isLoaded("ae2")) MolecularAssemblerAe2Provider.register();
     }
 
     private void addCreativeTabContents(BuildCreativeModeTabContentsEvent event) {
