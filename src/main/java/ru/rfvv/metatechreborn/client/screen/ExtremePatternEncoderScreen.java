aa384e84ead6be218f2d48b327fe6635f0004738
DARKIFOV <seleznev.ivan2005@gmail.com>
2026-08-02 16:12:44 +0500

    Redesign 9x9 pattern encoder GUI and add JEI hint

--- src/main/java/ru/rfvv/metatechreborn/client/screen/ExtremePatternEncoderScreen.java
+++ src/main/java/ru/rfvv/metatechreborn/client/screen/ExtremePatternEncoderScreen.java
@@ -4,23 +4,18 @@ import net.minecraft.client.gui.GuiGraphics;
 import net.minecraft.client.gui.components.Button;
 import net.minecraft.client.gui.screens.inventory.AbstractContainerScreen;
 import net.minecraft.network.chat.Component;
-import net.minecraft.resources.ResourceLocation;
 import net.minecraft.world.entity.player.Inventory;
 import org.jetbrains.annotations.NotNull;
-import ru.rfvv.metatechreborn.MetaTechReborn;
 import ru.rfvv.metatechreborn.blockentity.ExtremePatternEncoderBlockEntity;
 import ru.rfvv.metatechreborn.menu.ExtremePatternEncoderMenu;
 
 public final class ExtremePatternEncoderScreen extends AbstractContainerScreen<ExtremePatternEncoderMenu> {
-    private static final ResourceLocation TEXTURE = new ResourceLocation(
-            MetaTechReborn.MOD_ID, "textures/gui/molecular_assembler_9x9.png");
-
     public ExtremePatternEncoderScreen(ExtremePatternEncoderMenu menu, Inventory inventory, Component title) {
         super(menu, inventory, title);
-        imageWidth = 256;
-        imageHeight = 256;
-        inventoryLabelX = 8;
-        inventoryLabelY = 168;
+        imageWidth = 330;
+        imageHeight = 286;
+        inventoryLabelX = 10;
+        inventoryLabelY = 190;
     }
 
     @Override
@@ -28,10 +23,10 @@ public final class ExtremePatternEncoderScreen extends AbstractContainerScreen<E
         super.init();
         addRenderableWidget(Button.builder(Component.translatable("gui.metatech_reborn.encode_pattern"),
                         button -> sendButton(ExtremePatternEncoderMenu.ENCODE_BUTTON_ID))
-                .bounds(leftPos + 174, topPos + 17, 74, 18).build());
+                .bounds(leftPos + 224, topPos + 26, 90, 20).build());
         addRenderableWidget(Button.builder(Component.translatable("gui.metatech_reborn.clear_pattern_grid"),
                         button -> sendButton(ExtremePatternEncoderMenu.CLEAR_BUTTON_ID))
-                .bounds(leftPos + 174, topPos + 126, 74, 18).build());
+                .bounds(leftPos + 224, topPos + 136, 90, 20).build());
     }
 
     private void sendButton(int id) {
@@ -40,29 +35,45 @@ public final class ExtremePatternEncoderScreen extends AbstractContainerScreen<E
         }
     }
 
-    @Override public void render(@NotNull GuiGraphics graphics, int mouseX, int mouseY, float partialTick) {
+    @Override
+    public void render(@NotNull GuiGraphics graphics, int mouseX, int mouseY, float partialTick) {
         renderBackground(graphics);
         super.render(graphics, mouseX, mouseY, partialTick);
         renderTooltip(graphics, mouseX, mouseY);
     }
 
-    @Override protected void renderBg(@NotNull GuiGraphics graphics, float partialTick, int mouseX, int mouseY) {
-        graphics.blit(TEXTURE, leftPos, topPos, 0, 0, imageWidth, imageHeight, 512, 512);
-        graphics.fill(leftPos + 173, topPos + 49, leftPos + 249, topPos + 119, 0xAA07171D);
-        graphics.fill(leftPos + 189, topPos + 53, leftPos + 208, topPos + 73, 0xFF29434B);
-        graphics.fill(leftPos + 189, topPos + 91, leftPos + 208, topPos + 111, 0xFF29434B);
-        graphics.fill(leftPos + 214, topPos + 80, leftPos + 240, topPos + 84,
+    @Override
+    protected void renderBg(@NotNull GuiGraphics g, float partialTick, int mouseX, int mouseY) {
+        MetaTechGui.background(g, leftPos, topPos, imageWidth, imageHeight);
+        MetaTechGui.panel(g, leftPos + 6, topPos + 20, 166, 166);
+        MetaTechGui.panel(g, leftPos + 180, topPos + 6, 144, 180);
+        MetaTechGui.panel(g, leftPos + 6, topPos + 196, 166, 84);
+
+        MetaTechGui.grid(g, leftPos + 10, topPos + 26, 9, 9, 0xFF48BFE3);
+        MetaTechGui.grid(g, leftPos + 10, topPos + 202, 9, 3, 0xFF73879A);
+        MetaTechGui.grid(g, leftPos + 10, topPos + 260, 9, 1, 0xFF73879A);
+        MetaTechGui.slot(g, leftPos + 198, topPos + 64, 0xFF7A5DE8);
+        MetaTechGui.slot(g, leftPos + 198, topPos + 106, 0xFF48BFE3);
+
+        g.fill(leftPos + 224, topPos + 78, leftPos + 306, topPos + 88, 0xFF03090D);
+        g.fill(leftPos + 225, topPos + 79, leftPos + 305, topPos + 87,
                 menu.hasValidRecipe() ? 0xFF4EE08A : 0xFFFF4F67);
     }
 
-    @Override protected void renderLabels(@NotNull GuiGraphics graphics, int mouseX, int mouseY) {
-        graphics.drawString(font, title, 8, 4, 0xEAF8FF, false);
-        graphics.drawString(font, playerInventoryTitle, inventoryLabelX, inventoryLabelY, 0xBBD5E7, false);
-        graphics.drawString(font, Component.translatable("gui.metatech_reborn.blank_pattern"),
-                174, 43, 0xBBD5E7, false);
-        graphics.drawString(font, Component.translatable("gui.metatech_reborn.encoded_pattern"),
-                174, 111, 0xBBD5E7, false);
-        graphics.drawString(font, statusText(menu.getStatus()), 174, 149, statusColor(menu.getStatus()), false);
+    @Override
+    protected void renderLabels(@NotNull GuiGraphics g, int mouseX, int mouseY) {
+        g.drawString(font, title, 10, 8, 0xEAF8FF, false);
+        g.drawString(font, playerInventoryTitle, inventoryLabelX, inventoryLabelY, 0xBBD5E7, false);
+
+        g.drawString(font, Component.translatable("gui.metatech_reborn.blank_pattern"),
+                190, 52, 0xBBD5E7, false);
+        g.drawString(font, Component.translatable("gui.metatech_reborn.encoded_pattern"),
+                190, 94, 0xBBD5E7, false);
+        MetaTechGui.drawWrapped(g, font, statusText(menu.getStatus()),
+                188, 162, 128, statusColor(menu.getStatus()), 2);
+        MetaTechGui.drawWrapped(g, font,
+                Component.translatable("gui.metatech_reborn.encoder.jei_hint"),
+                188, 112, 128, 0x9CCBFF, 2);
     }
 
     private static Component statusText(int status) {
