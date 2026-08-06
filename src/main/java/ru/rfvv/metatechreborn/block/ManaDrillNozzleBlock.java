5c9d73bc3f7835fc6eca5d404af511c9990cbc09
DARKIFOV <seleznev.ivan2005@gmail.com>
2026-08-06 10:09:00 +0500

    Hide assembled drill nozzle model

--- src/main/java/ru/rfvv/metatechreborn/block/ManaDrillNozzleBlock.java
+++ src/main/java/ru/rfvv/metatechreborn/block/ManaDrillNozzleBlock.java
@@ -1,28 +1,37 @@
 package ru.rfvv.metatechreborn.block;
 
+import net.minecraft.core.BlockPos;
 import net.minecraft.core.Direction;
 import net.minecraft.world.item.context.BlockPlaceContext;
+import net.minecraft.world.level.Level;
 import net.minecraft.world.level.block.HorizontalDirectionalBlock;
 import net.minecraft.world.level.block.Mirror;
 import net.minecraft.world.level.block.Rotation;
 import net.minecraft.world.level.block.state.BlockState;
 import net.minecraft.world.level.block.state.StateDefinition;
+import net.minecraft.world.level.block.state.properties.BooleanProperty;
 import net.minecraft.world.level.block.state.properties.DirectionProperty;
 import org.jetbrains.annotations.NotNull;
 import org.jetbrains.annotations.Nullable;
+import ru.rfvv.metatechreborn.multiblock.ManaDrillStructure;
 
 /** Directional nozzle so the drill head can be turned to match the multiblock controller. */
 public final class ManaDrillNozzleBlock extends HorizontalDirectionalBlock {
     public static final DirectionProperty FACING = HorizontalDirectionalBlock.FACING;
+    public static final BooleanProperty FORMED = BooleanProperty.create("formed");
 
     public ManaDrillNozzleBlock(Properties properties) {
         super(properties);
-        registerDefaultState(stateDefinition.any().setValue(FACING, Direction.NORTH));
+        registerDefaultState(stateDefinition.any()
+                .setValue(FACING, Direction.NORTH)
+                .setValue(FORMED, false));
     }
 
     @Override
     public @Nullable BlockState getStateForPlacement(BlockPlaceContext context) {
-        return defaultBlockState().setValue(FACING, context.getHorizontalDirection().getOpposite());
+        return defaultBlockState()
+                .setValue(FACING, context.getHorizontalDirection().getOpposite())
+                .setValue(FORMED, false);
     }
 
     @Override
@@ -37,6 +46,24 @@ public final class ManaDrillNozzleBlock extends HorizontalDirectionalBlock {
 
     @Override
     protected void createBlockStateDefinition(StateDefinition.Builder<net.minecraft.world.level.block.Block, BlockState> builder) {
-        builder.add(FACING);
+        builder.add(FACING, FORMED);
+    }
+
+    @Override
+    public void onPlace(@NotNull BlockState state, @NotNull Level level, @NotNull BlockPos pos,
+                        @NotNull BlockState oldState, boolean isMoving) {
+        super.onPlace(state, level, pos, oldState, isMoving);
+        if (!oldState.is(state.getBlock())) {
+            ManaDrillStructure.notifyControllers(level, pos);
+        }
+    }
+
+    @Override
+    public void onRemove(@NotNull BlockState state, @NotNull Level level, @NotNull BlockPos pos,
+                         @NotNull BlockState newState, boolean isMoving) {
+        if (!state.is(newState.getBlock())) {
+            ManaDrillStructure.notifyControllers(level, pos);
+        }
+        super.onRemove(state, level, pos, newState, isMoving);
     }
 }
